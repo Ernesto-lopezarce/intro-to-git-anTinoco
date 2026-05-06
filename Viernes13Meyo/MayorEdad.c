0c6b2cf2790912bbdd585093e37977fbd2e84ccf
anTinoco <angel.tinoco8140@alumnos.udg.mx>
2026-05-06 08:21:58 -0600

    primer commit

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,17 @@
+//Angel Ulises Tinoco Aguilar
+//Ez
+
 #include <stdio.h>
+// Código perrón
+int main()
+{
+    int edad;
+    printf("Indica tu edad\n");
+    scanf("%d", &edad);
+    if(edad>=18)
+        printf("Señor, usted es mayor de edad");
+    else
+        printf("Jaja chamaco miado");
+
+    return 0;
+}
